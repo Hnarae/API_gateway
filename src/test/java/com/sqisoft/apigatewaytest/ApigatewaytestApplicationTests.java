9c4a0ef74968cc5b6d0d99c21d6af422f1782fd7
Hnarae <nr.han@sqisoft.com>
2021-08-30 17:05:05 +0900

    api gateway source

---
+++ src/test/java/com/sqisoft/apigatewaytest/ApigatewaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqisoft.apigatewaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
